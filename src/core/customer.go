4b7d708b9257eaf0d92c94b4dad81c8cc862ab60
= <=>
2023-06-07 17:46:35 +0330

    feat[1]: customer core object + customer repository are implemented

---
+++ src/core/customer.go
@@ -0,0 +1,16 @@
+package core
+
+// Customer Core Object
+type Customer struct {
+	Id          string
+	Name        string
+	City        string
+	Zipcode     string
+	DateofBirth string
+	Status      string
+}
+
+// Customer Secondary Port
+type CustomerRepository interface {
+	FindAll() ([]Customer, error)
+}
